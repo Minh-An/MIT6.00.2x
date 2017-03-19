1e284785c164923ce4d7d6696e18bd41c4416653
An Doan <minh.an.doan@gmail.com>
2017-03-18 18:41:42 -0700

    Add a function to generate all combinations of items into two bags.

--- l2/powerset.py
+++ l2/powerset.py
@@ -1,12 +1,36 @@
 #generate power set of items 
 def powerSet(items):
     N = len(items)
+    #enumerate through all possible combinations of 2**N 
     for i in range(2**N):
         combo = []
         for j in range(N):
+            #test the jth bit of integer i, i >> j is equal to i // (2**j)
             if (i >> j) % 2 == 1:
                 combo.append(items[j])
         yield combo
         
-for combo in powerSet(['a','b','c']):
-    print(combo)
+#for combo in powerSet(['a','b','c']):
+#    print(combo)
+    
+def yieldAllCombos(items):
+    """
+        Generates all combinations of N items into two bags, whereby each 
+        item is in one or zero bags.
+
+        Yields a tuple, (bag1, bag2), where each bag is represented as a list 
+        of which item(s) are in each bag.
+    """
+    N = len(items)
+    #enumerate through all possible combinations of 3**N 
+    for i in range(3**N):
+        combo = ([],[])
+        for j in range(N):
+            if (i // (3**j)) % 3 == 1:
+                combo[0].append(items[j])
+            elif (i // (3**j)) % 3 == 2:
+                combo[1].append(items[j])
+        yield combo
+#
+#for combo in yieldAllCombos(['a', 'b']):
+#    print(combo)
